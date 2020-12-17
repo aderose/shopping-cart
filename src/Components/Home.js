5a9c3c9ad0be23b55dfe9c8736070fd630cca513
aderose <sandro.derose28@yahoo.com>
2020-12-17 18:31:28 +0000

    Structure home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = (props) => {
-  return <div>THIS IS THE HOME PAGE</div>;
+import '../styles/Home.css';
+
+const Home = () => {
+  return (
+    <div className="container home">
+      <h1 className="title">Vintage Champagne</h1>
+      <p>With hundreds of Champagnes available, you'll be spoilt for choice!</p>
+      <Link to="/shop" className="btn">
+        Shop Now
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
