a5b6e376ceec2eb2c88656ff6b37033008f9a593
aderose <sandro.derose28@yahoo.com>
2020-12-17 18:30:25 +0000

    Adjust App structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink, Link } from 'react-router-dom';
+
 import Home from './Components/Home';
 import Shop from './Components/Shop';
 import About from './Components/About';
-import { BrowserRouter, Switch, Route, Link, NavLink } from 'react-router-dom';
 
 import './styles/App.css';
 
 const App = () => {
   return (
-    <div
-      className="
-  bg-no-repeat bg-bottom bg-cover h-screen"
-      style={{
-        backgroundImage: `url("https://images.unsplash.com/photo-1456441240751-2a368527053f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2400")`,
-      }}
-    >
+    <div className="app">
       <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link>
+        <nav className="container navigation">
+          <Link className="nav-home" to="/">
+            Home
+          </Link>
           <ul>
             <li>
               <NavLink to="/shop">Shop</NavLink>
@@ -29,8 +26,8 @@ const App = () => {
         </nav>
         <Switch>
           <Route exact from="/" component={Home} />
-          <Route exact from="/shop" component={Shop} />
-          <Route exact from="/about" component={About} />
+          <Route from="/shop" component={Shop} />
+          <Route from="/about" component={About} />
         </Switch>
       </BrowserRouter>
     </div>
