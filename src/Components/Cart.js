d338a18ad9d9b70b841738bbced276d75488da0f
aderose <sandro.derose28@yahoo.com>
2020-12-20 19:43:22 +0000

    Create & Style shopping cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
-const Cart = (props) => {
-  return <div className="container">THIS IS THE CART</div>;
+import CartItem from './CartItem';
+
+import '../styles/Cart.css';
+
+const Cart = ({ basket, grandTotal, onChange }) => {
+  return (
+    <div className="container card cart">
+      <h1>Your Shopping Basket</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>#</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {basket.map(({ id, title, price, quantity, total }) => (
+            <CartItem
+              title={title}
+              price={price}
+              quantity={quantity}
+              total={total}
+              onChange={onChange(id)}
+              key={id}
+            />
+          ))}
+        </tbody>
+      </table>
+      <div className="total">
+        <p>
+          <strong>Grand Total: </strong>
+          <span>£{grandTotal.toFixed(2)}</span>
+        </p>
+        <button className="btn">Proceed to Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
