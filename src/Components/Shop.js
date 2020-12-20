3fcb29b7767eb0194feb4e91603910f5b85b422d
aderose <sandro.derose28@yahoo.com>
2020-12-20 11:21:51 +0000

    Use product component to test a few products

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Product from './Product';
+
+import '../styles/Shop.css';
 
 const Shop = (props) => {
-  return <div className="container">THIS IS THE SHOP PAGE</div>;
+  return (
+    <div className="container shop">
+      <h1>Our Products:</h1>
+      <Product
+        link="https://img2.pngio.com/champagne-bottle-png-transparent-image-moet-champagne-bottle-png-1200_1200.png"
+        alt="Moet"
+        title="Moet & Chandon Brut Imperial Non Vintage Champagne 75Cl"
+        price="£29.00"
+      />
+      <Product
+        link="https://img2.pngio.com/champagne-bottle-png-transparent-image-moet-champagne-bottle-png-1200_1200.png"
+        alt="Moet"
+        title="Moet & Chandon Brut Imperial Non Vintage Champagne 75Cl"
+        price="£29.00"
+      />
+      <Product
+        link="https://img2.pngio.com/champagne-bottle-png-transparent-image-moet-champagne-bottle-png-1200_1200.png"
+        alt="Moet"
+        title="Moet & Chandon Brut Imperial Non Vintage Champagne 75Cl"
+        price="£29.00"
+      />
+    </div>
+  );
 };
 
 export default Shop;
