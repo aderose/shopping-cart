3158da7e4c3778c6b7800ffe3763efeb04961e4d
aderose <sandro.derose28@yahoo.com>
2020-12-20 19:59:52 +0000

    Capture form quantity as state and pass through callback on submit

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/Product.css';
 
-const Product = ({ link, alt, title, price }) => {
+const Product = ({ id, link, alt, title, price, submit }) => {
+  const [value, setValue] = useState(1);
+
+  const updateValue = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    submit({ id, quantity: parseInt(value) });
+  };
+
   return (
     <div className="product card">
       <img src={link} alt={alt} />
       <div className="product-details">
         <h2>{title}</h2>
         <p>{price}</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="number"
             name="amount"
@@ -17,6 +28,8 @@ const Product = ({ link, alt, title, price }) => {
             min="1"
             max="19"
             autoComplete="off"
+            value={value}
+            onChange={updateValue}
           />
           <input type="submit" value="Add" className="btn" />
         </form>
