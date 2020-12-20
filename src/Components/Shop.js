0bb741bbe11e8656795571eb721f00343f496f53
aderose <sandro.derose28@yahoo.com>
2020-12-20 20:00:33 +0000

    Funnel form submission state to App component via callback

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -3,12 +3,20 @@ import Product from './Product';
 
 import '../styles/Shop.css';
 
-const Shop = ({ products }) => {
+const Shop = ({ products, addProduct }) => {
   return (
     <div className="container shop">
       <h1>Our Products:</h1>
       {products.map(({ id, link, alt, title, price }) => (
-        <Product link={link} alt={alt} title={title} price={price} key={id} />
+        <Product
+          id={id}
+          link={link}
+          alt={alt}
+          title={title}
+          price={price}
+          submit={addProduct}
+          key={id}
+        />
       ))}
     </div>
   );
